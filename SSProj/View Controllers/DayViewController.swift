6a2f94308c17892b526fd10238533a486bbcccf4
Cassandra Kane <cassandra.kane@gmail.com>
2015-07-14 16:13:50 -0400

    work on DayVC

--- SSProj/View Controllers/DayViewController.swift
+++ SSProj/View Controllers/DayViewController.swift
@@ -13,10 +13,16 @@ import RealmSwift
 class DayViewController: UIViewController {
 
     var user: User = User()
+    var dates: [String] = []
+    var totLikesPerDay: [String : [Int]] = [ : ]
+    var aveLikesPerDay: [String: Double] = [ : ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUser()
+        for index in 1...7 {
+            totLikesPerDay["\(index)"] = []
+        }
         // Do any additional setup after loading the view.
         
     }
@@ -24,8 +30,13 @@ class DayViewController: UIViewController {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
+        createDates()
+        createDaysWithLikes()
+        createAverages()
+        
+        println("Day Opted")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -34,6 +45,57 @@ class DayViewController: UIViewController {
     func setUser() {
         let realm = Realm()
         user = realm.objects(User).first!
+    }
+    
+    
+    func createDates() {
+        var i: Int = 0
+        for (i = 0; i < user.posts.count; i++) {
+            let post = user.posts[i]
+            let date = post.getDate()
+            dates.append(date.description)
+        }
+    }
+    
+    func createDaysWithLikes() {
+        var i: Int = 0
+        for (i = 0; i < user.posts.count; i++) {
+            let post = user.posts[i]
+            let date = dates[i]
+            let rangeOfDate = Range(start: (advance(date.startIndex, 0)), end: (advance(date.startIndex, 10)))
+            let dateString = date.substringWithRange(rangeOfDate)
+            let dayOfWeek: Int = getDayOfWeek(dateString)
+            let numOfLikes = post.numOfLikes
+            totLikesPerDay["\(dayOfWeek)"]!.append(numOfLikes)
+        }
+    }
+    
+    func createAverages() {
+        for i in 1...7 {
+            let likes: [Int] = totLikesPerDay["\(i)"]!
+            var sum: Int = 0
+            var j: Int
+            for (j = 0; j < likes.count; j++) {
+                sum += likes[j]
+            }
+            let average: Double = Double(sum)/Double(likes.count)
+            
+            aveLikesPerDay["\(i)"] = average
+        
+        }
+    }
+    
+    
+    func getDayOfWeek(date:String) -> Int {
+        
+        let formatter  = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        let todayDate = formatter.dateFromString(date)!
+        let myCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        let myComponents = myCalendar.components(.CalendarUnitWeekday, fromDate: todayDate)
+        let weekDay = myComponents.weekday
+        return weekDay
+        
     }
     
     
