a7e804921b3e630f78f5396683ef9d2734cf3ee8
Cassandra Kane <cassandra.kane@gmail.com>
2015-07-20 17:02:23 -0400

    imported new pods

---
+++ SSProj/SSProj-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  SSProj-Bridging-Header.h
+//  SSProj
+//
+//  Created by Cassandra Kane on 7/20/15.
+//  Copyright (c) 2015 Cassandra Kane. All rights reserved.
+//
+
+#ifndef SSProj_SSProj_Bridging_Header_h
+#define SSProj_SSProj_Bridging_Header_h
+
+#import <TSMessages/TSMessage.h>
+#import <LBBlurredImage/UIImageView+LBBlurredImage.h>
+
+#endif
